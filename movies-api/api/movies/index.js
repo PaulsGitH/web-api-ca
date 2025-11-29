5dffddf227ba83ed73f3d1d368e134d7d737a969
PaulsGitH <paullydee0@gmail.com>
2025-11-29 19:17:32 +0000

    Added upcoming movies endpoint to Movies API

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import { getMovies, getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router();
 
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+router.get('/upcoming', asyncHandler(async (req, res) => {
+  const upcomingMovies = await getUpcomingMovies();
+  res.status(200).json(upcomingMovies);
 }));
 
 
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -11,3 +11,16 @@ export const getMovies = async () => {
 
     return await response.json();
 };
+
+
+export const getUpcomingMovies = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
+
+  if (!response.ok) {
+    throw new Error((await response.json()).message);
+  }
+
+  return await response.json();
+};
