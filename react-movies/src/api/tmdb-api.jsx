bd10fcb2b8a6dc3da554dcdb15c06fff06cd0c85
PaulsGitH <paullydee0@gmail.com>
2025-11-29 20:47:55 +0000

    Now playing movies now using Movies API

--- react-movies/src/api/tmdb-api.jsx
+++ react-movies/src/api/tmdb-api.jsx
@@ -152,7 +152,7 @@ export const getTopRatedPaged = ({ queryKey }) => {
 
 export const getNowPlayingMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+    `http://localhost:8080/api/movies/now_playing`
   )
     .then((response) => {
       if (!response.ok) {
