28588fb951a3bf64820cedfd52c010766eb441fb
PaulsGitH <paullydee0@gmail.com>
2025-11-29 19:30:26 +0000

    Added genres endpoint to Movies API

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies, getUpcomingMovies } from '../tmdb-api';
+import { getMovies, getUpcomingMovies, getGenres } from '../tmdb-api';
+
 
 const router = express.Router();
 
@@ -12,6 +13,11 @@ router.get('/discover', asyncHandler(async (req, res) => {
 router.get('/upcoming', asyncHandler(async (req, res) => {
   const upcomingMovies = await getUpcomingMovies();
   res.status(200).json(upcomingMovies);
+}));
+
+router.get('/genres', asyncHandler(async (req, res) => {
+  const genres = await getGenres();
+  res.status(200).json(genres);
 }));
 
 
