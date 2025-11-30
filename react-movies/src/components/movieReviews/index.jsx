b1fb1fdc23c5e81a50ff84c2dbcb4a896aa4ab7a
PaulsGitH <paullydee0@gmail.com>
2025-11-30 19:48:29 +0000

    Improved movie reviews drawer to clearly handle missing TMDB reviews and only show write review actions for signed in users

--- react-movies/src/components/movieReviews/index.jsx
+++ react-movies/src/components/movieReviews/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,13 +6,19 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { Link } from "react-router";
+import { useQuery } from "@tanstack/react-query";
 import { getMovieReviews } from "../../api/tmdb-api";
 import { excerpt } from "../../util";
-import { useQuery } from "@tanstack/react-query";
 import Spinner from "../spinner";
+import { AuthContext } from "../../contexts/authContext";
 
 export default function MovieReviews({ movie }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
   const { data, error, isPending, isError } = useQuery({
     queryKey: ["reviews", { id: movie.id }],
     queryFn: getMovieReviews,
@@ -28,38 +34,79 @@ export default function MovieReviews({ movie }) {
 
   const reviews = data?.results || [];
 
+  if (reviews.length === 0) {
+    return (
+      <Paper sx={{ p: 3 }}>
+        <Typography variant="h6" sx={{ mb: 1 }}>
+          No reviews from TMDB for this movie yet.
+        </Typography>
+        {isAuthenticated ? (
+          <Button
+            variant="contained"
+            size="small"
+            component={Link}
+            to="/reviews/form"
+            state={{ movieId: movie.id }}
+          >
+            Write a review
+          </Button>
+        ) : (
+          <Typography variant="body2">
+            Sign in to write a review.
+          </Typography>
+        )}
+      </Paper>
+    );
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 550 }} aria-label="reviews table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Author</TableCell>
-            <TableCell align="center">Excerpt</TableCell>
-            <TableCell align="right">More</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {reviews.map((r) => (
-            <TableRow key={r.id}>
-              <TableCell component="th" scope="row">
-                {r.author}
-              </TableCell>
-              <TableCell>{excerpt(r.content)}</TableCell>
-              <TableCell>
-                <Link
-                  to={`/reviews/${r.id}`}
-                  state={{
-                    review: r,
-                    movie: movie,
-                  }}
-                >
-                  Full Review
-                </Link>
-              </TableCell>
+    <Box sx={{ p: 1.5 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 550 }} aria-label="reviews table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Author</TableCell>
+              <TableCell align="center">Excerpt</TableCell>
+              <TableCell align="right">More</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {reviews.map((r) => (
+              <TableRow key={r.id}>
+                <TableCell component="th" scope="row">
+                  {r.author}
+                </TableCell>
+                <TableCell>{excerpt(r.content)}</TableCell>
+                <TableCell>
+                  <Link
+                    to={`/reviews/${r.id}`}
+                    state={{
+                      review: r,
+                      movie: movie,
+                    }}
+                  >
+                    Full Review
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {isAuthenticated && (
+        <Box sx={{ mt: 2 }}>
+          <Button
+            variant="contained"
+            size="small"
+            component={Link}
+            to="/reviews/form"
+            state={{ movieId: movie.id }}
+          >
+            Write your own review
+          </Button>
+        </Box>
+      )}
+    </Box>
   );
 }
